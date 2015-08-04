6c7b8324fb1e8a37f815d9939f787911026da545
shaovoon <shaovoon@gmail.com>
2015-08-04 20:40:27 +0800

    Added string streams

--- MiniCSV/example.cpp
+++ MiniCSV/example.cpp
@@ -12,31 +12,53 @@ struct Product
 
 int main()
 {
-	csv::ofstream os("products.txt", std::ios_base::out);
-	os.set_delimiter('\t');
-	if(os.is_open())
+	// test file streams
 	{
+		csv::ofstream os("products.txt");
+		os.set_delimiter('\t');
+		if (os.is_open())
+		{
+			Product product("Shampoo", 200, 15.0f);
+			os << product.name << product.qty << product.price << NEWLINE;
+			Product product2("Soap", 300, 6.0f);
+			os << product2.name << product2.qty << product2.price << NEWLINE;
+		}
+		os.flush();
+		os.close();
+
+		csv::ifstream is("products.txt");
+		is.set_delimiter('\t');
+		if (is.is_open())
+		{
+			Product temp;
+			while (is.read_line())
+			{
+				is >> temp.name >> temp.qty >> temp.price;
+				// display the read items
+				std::cout << temp.name << "," << temp.qty << "," << temp.price << std::endl;
+			}
+		}
+	}
+
+	// test string streams
+	{
+		csv::ostringstream os;
+		os.set_delimiter('\t');
 		Product product("Shampoo", 200, 15.0f);
 		os << product.name << product.qty << product.price << NEWLINE;
 		Product product2("Soap", 300, 6.0f);
 		os << product2.name << product2.qty << product2.price << NEWLINE;
-	}
-	os.flush();
-	os.close();
 
-	csv::ifstream is("products.txt", std::ios_base::in);
-	is.set_delimiter('\t');
-	if(is.is_open())
-	{
+		csv::istringstream is(os.get_text().c_str());
+		is.set_delimiter('\t');
 		Product temp;
-		while(is.read_line())
+		while (is.read_line())
 		{
 			is >> temp.name >> temp.qty >> temp.price;
 			// display the read items
 			std::cout << temp.name << "," << temp.qty << "," << temp.price << std::endl;
 		}
 	}
-
 	return 0;
 }
 
--- MiniCSV/minicsv.h
+++ MiniCSV/minicsv.h
@@ -1,5 +1,5 @@
 // The MIT License (MIT)
-// Minimalistic CSV Streams 1.4
+// Minimalistic CSV Streams 1.6
 // Copyright (C) 2014, by Wong Shao Voon (shaovoon@yahoo.com)
 //
 // http://opensource.org/licenses/MIT
@@ -9,6 +9,7 @@
 //               added: skip_1st_line and skip_line functions to ifstream class
 // version 1.4 : Removed the use of smart ptr.
 // version 1.5 : Performance increase on writing without flushing every line.
+// version 1.6 : Add string streams
 
 //#define USE_BOOST_LEXICAL_CAST
 
@@ -34,14 +35,14 @@ namespace csv
 		ifstream() : str(""), pos(0), delimiter(',')
 		{
 		}
-		ifstream(const char * file, std::ios_base::openmode mode)
+		ifstream(const char * file)
 		{
-			open(file, mode);
+			open(file);
 		}
-		void open(const char * file, std::ios_base::openmode mode)
+		void open(const char * file)
 		{
 			init();
-			istm.open(file, mode);
+			istm.open(file, std::ios_base::in);
 		}
 		void init()
 		{
@@ -128,14 +129,14 @@ namespace csv
 		ofstream() : after_newline(true), delimiter(',')
 		{
 		}
-		ofstream(const char * file, std::ios_base::openmode mode)
+		ofstream(const char * file)
 		{
-			open(file, mode);
+			open(file);
 		}
-		void open(const char * file, std::ios_base::openmode mode)
+		void open(const char * file)
 		{
 			init();
-			ostm.open(file, mode);
+			ostm.open(file, std::ios_base::out);
 		}
 		void init()
 		{
@@ -228,6 +229,167 @@ inline csv::ofstream& operator << (csv::ofstream& ostm, const char& val)
 	}
 	else
 		ostm.get_ofstream() << val;
+
+	return ostm;
+}
+
+namespace csv
+{
+
+class istringstream
+{
+public:
+	istringstream(const char * text)
+		: str("")
+		, pos(0)
+		, delimiter(',')
+	{
+		istm.str(text);
+	}
+	void set_delimiter(char delimiter_)
+	{
+		delimiter = delimiter_;
+	}
+	char get_delimiter() const
+	{
+		return delimiter;
+	}
+	void skip_line()
+	{
+		std::getline(istm, str);
+		pos = 0;
+	}
+	bool read_line()
+	{
+		this->str = "";
+		if (!istm.eof())
+		{
+			std::getline(istm, this->str);
+			pos = 0;
+
+			if (this->str.empty())
+				return false;
+
+			return true;
+		}
+		return false;
+	}
+	std::string get_delimited_str()
+	{
+		std::string str = "";
+		char ch = '\0';
+		do
+		{
+			if (pos >= this->str.size())
+			{
+				this->str = "";
+
+				return str;
+			}
+
+			ch = this->str[pos];
+			++(pos);
+			if (ch == delimiter || ch == '\r' || ch == '\n')
+				break;
+
+			str += ch;
+		} while (true);
+
+		return str;
+	}
+private:
+	std::istringstream istm;
+	std::string str;
+	size_t pos;
+	char delimiter;
+};
+
+class ostringstream
+{
+public:
+
+	ostringstream() : after_newline(true), delimiter(',')
+	{
+	}
+	void set_delimiter(char delimiter_)
+	{
+		delimiter = delimiter_;
+	}
+	char get_delimiter() const
+	{
+		return delimiter;
+	}
+	void set_after_newline(bool after_newline_)
+	{
+		after_newline = after_newline_;
+	}
+	bool get_after_newline() const
+	{
+		return after_newline;
+	}
+	std::ostringstream& get_ostringstream()
+	{
+		return ostm;
+	}
+	std::string get_text()
+	{
+		return ostm.str();
+	}
+
+private:
+	std::ostringstream ostm;
+	bool after_newline;
+	char delimiter;
+};
+
+
+} // ns csv
+
+template<typename T>
+csv::istringstream& operator >> (csv::istringstream& istm, T& val)
+{
+	std::string str = istm.get_delimited_str();
+
+#ifdef USE_BOOST_LEXICAL_CAST
+	val = boost::lexical_cast<T>(str);
+#else
+	std::istringstream is(str);
+	is >> val;
+#endif
+
+	return istm;
+}
+template<>
+inline csv::istringstream& operator >> (csv::istringstream& istm, std::string& val)
+{
+	val = istm.get_delimited_str();
+
+	return istm;
+}
+
+template<typename T>
+csv::ostringstream& operator << (csv::ostringstream& ostm, const T& val)
+{
+	if (!ostm.get_after_newline())
+		ostm.get_ostringstream() << ostm.get_delimiter();
+
+	ostm.get_ostringstream() << val;
+
+	ostm.set_after_newline(false);
+
+	return ostm;
+}
+template<>
+inline csv::ostringstream& operator << (csv::ostringstream& ostm, const char& val)
+{
+	if (val == NEWLINE)
+	{
+		ostm.get_ostringstream() << NEWLINE;
+
+		ostm.set_after_newline(true);
+	}
+	else
+		ostm.get_ostringstream() << val;
 
 	return ostm;
 }
